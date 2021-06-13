be19c35a7eb7b255ade3204c5a6a0b71d9df670b
Suvel <suvelrathneswar08@gmail.com>
2021-06-13 21:35:38 +0530

    Added Modal Transition

--- src/component/Modal.js
+++ src/component/Modal.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import './Modal.scss'
-
+import { animated, config, useTransition } from 'react-spring'
 const Modal = ({ children, show }) => {
-    return (
-        <div className={`modal__container show-${show}`} >
-            {children}
-        </div >
+
+    const transitions = useTransition(show, {
+        from: { opacity: 0, transform: 'scale(0) translate(-100%, -100%)' },
+        enter: { opacity: 1, transform: 'scale(1) translate(-50%, -50%)' },
+        leave: { opacity: 0, transform: 'scale(0) translate(-100%, -100%)' },
+        config: config.gentle,
+    })
+    return transitions(
+        (styles, item) => item && (
+            <animated.div style={styles} className={`modal__container`} >
+                {children}
+            </animated.div >
+        )
     )
 }
 
