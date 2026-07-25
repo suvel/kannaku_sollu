8f93b369d562313865b4e2534c2bfc9a11813e6f
suvel <find.suvel@gmail.com>
2026-07-25 21:25:57 +0530

    Add splash screen and update logo

--- src/index.js
+++ src/index.js
@@ -5,3 +5,10 @@ import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
+
+document.getElementById("splash-screen")?.remove();
+
+// Failsafe: force-hide the splash screen after 30s in case mounting hangs.
+setTimeout(() => {
+  document.getElementById("splash-screen")?.remove();
+}, 30000);
